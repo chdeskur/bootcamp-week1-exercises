af5994005e5f9075cd2d2f5a6b1fe532fa5612fc
chdeskur <chdeskur@gmail.com>
2021-01-09 01:56:40 -0500

    Updates

---
+++ precache-manifest.696590ba60db09876615e3ad030ea932.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c129e0158d1cd2b3cfcb72aae56228",
+    "url": "/bootcamp-week1-exercises/personal-assistant/index.html"
+  },
+  {
+    "revision": "7a9b603bbc4d7d0dceb3",
+    "url": "/bootcamp-week1-exercises/personal-assistant/static/js/2.dc53fcca.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/bootcamp-week1-exercises/personal-assistant/static/js/2.dc53fcca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8a2806d50908f781a9",
+    "url": "/bootcamp-week1-exercises/personal-assistant/static/js/main.e7f6da36.chunk.js"
+  },
+  {
+    "revision": "bdb39e1591c6bee05bb6",
+    "url": "/bootcamp-week1-exercises/personal-assistant/static/js/runtime-main.27758e09.js"
+  }
+]);
